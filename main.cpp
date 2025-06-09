6e26851e1f4ef17be567775db1b4f4d9f3a8799c
nakshayan <animalat@uwaterloo.ca>
2025-06-09 00:04:49 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+int main() {
+    /* max cx
+     * s.t. 
+     * Ax = b
+     * x >= 0
+    */
+
+    
+    std::vector<int> c;
+    std::vector<int> b;
+    // vector of columns
+    std::vector<std::vector<int>> A;
+
+    
+
+    return 0;
+}
