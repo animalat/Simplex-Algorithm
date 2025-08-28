aad947fd0b67c2cfc888e78bf07c67a77c5abe48
nakshayan <animalat@uwaterloo.ca>
2025-08-28 02:41:12 -0400

    input menu

--- frontend/simplex-solver-frontend/src/app/page.tsx
+++ frontend/simplex-solver-frontend/src/app/page.tsx
@@ -1,8 +1,11 @@
+import InputCard from "@/components/ui/inputCard"
+
 export default function Home() {
   return (
     <div>
       <h1>hi this is a test</h1>
       <p>testing 1 2 3</p>
+      <InputCard />
     </div>
   );
 }
---
+++ frontend/simplex-solver-frontend/src/components/ui/inputCard.tsx
@@ -0,0 +1,42 @@
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { TextArea } from "./TextArea"
+
+export default function InputCard() {
+    const defaultInput = "let   x1;\nlet   x2;\nmax 3 * x1 + 4 * x2;\ns.t.   x1 + x2 <= 5;\n       x1 >= 0;\n       x2>=0;"
+    return (
+        <div className="flex justify-center">
+            <Card className="w-[75vw]">
+                <CardHeader>
+                    <CardTitle>Linear Program Solver</CardTitle>
+                    <CardDescription>Enter a Linear Program below to solve</CardDescription>
+                    <CardAction>
+                        <Button variant="ghost">Example</Button>
+                    </CardAction>
+                </CardHeader>
+                <CardContent>
+                    <form>
+                        <TextArea
+                            id="simplexInput"
+                            className="h-[400px]"
+                            defaultValue={defaultInput}
+                        />
+                    </form>
+                </CardContent>
+                <CardFooter>
+                    <Button type="submit" className="w-full">
+                        Solve
+                    </Button>
+                </CardFooter>
+            </Card>
+        </div>
+    )
+}
