2e14e7c5e579a409345f20f2f6ff17842a5519ba
Akshayan <animalat@uwaterloo.ca>
2026-04-29 17:46:47 -0400

    continue w/ test harness

--- simplex_core/tests/test_simplex.py
+++ simplex_core/tests/test_simplex.py
@@ -1,18 +1,37 @@
 import os
+from typing import Iterator
 
-def get_files(path, extension):
+
+def get_files(path, extension) -> Iterator[str]:
     import glob
 
     for root, _, _ in os.walk(path):
-        for file_path in glob.iglob(root + "/*." + extension):
-            yield file_path
+        yield from glob.iglob(root + '/*.' + extension)
+
+
+def run_test(input_file: str, output_file: str) -> bool:
+    if not os.path.isfile(input_file):
+        print(f'Missing file: {input_file}')
+        return False
+    if not os.path.isfile(output_file):
+        print(f'Missing file: {output_file}')
+        return False
+    
+    return True
+
 
 def main() -> None:
-    INPUT_EXTENSION = "in"
-    OUTPUT_EXTENSION = "out"
+    INPUT_EXTENSION = 'in'
+    OUTPUT_EXTENSION = 'out'
 
-    for input_file in get_files("./", INPUT_EXTENSION):
+    for input_file in get_files('./', INPUT_EXTENSION):
         output_file = input_file.removesuffix(INPUT_EXTENSION) + OUTPUT_EXTENSION
+        if not run_test(input_file, output_file):
+            print(f'Failed test: {input_file}')
+            return
+
+    print(f'All tests passed!')
+
 
 if __name__ == '__main__':
     main()
